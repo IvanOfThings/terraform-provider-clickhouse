6dd7f349c2c1d9f919329acca4b4721825452a28
Daniel Moratalla <daniel.moratalla@lansweeper.com>
2025-06-17 08:56:46 +0200

    :recycle: updated allowed db level privileges adding flush logs and truncate table

--- pkg/resources/role/validators.go
+++ pkg/resources/role/validators.go
@@ -2,9 +2,10 @@ package resourcerole
 
 import (
 	"fmt"
+	"strings"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"strings"
 )
 
 var AllowedDbLevelPrivileges = []string{
@@ -21,6 +22,8 @@ var AllowedDbLevelPrivileges = []string{
 	"DROP VIEW",
 	"SHOW TABLES",
 	"dictGet",
+	"FLUSH LOGS",
+	"TRUNCATE TABLE",
 }
 
 var AllowedGlobalPrivileges = []string{
